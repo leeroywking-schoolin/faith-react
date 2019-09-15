a07cb1464c29642d6785c4fc18fce2aea841940d
Lee-Roy King <leeroywking@gmail.com>
2019-09-15 16:59:40 -0700

    build

--- faith-react/src/App.js
+++ faith-react/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hey Faith
+          Hey Faith!
         </p>
       </header>
     </div>
